fb5d80fb8d62fff0aa8ba938e2524f677844fa6d
priyanjanasriva <priyanjanasriva@gmail.com>
2022-07-12 11:03:28 +0530

    3sum

--- 3sum.cpp
+++ 3sum.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         
-        set<vector<int>> s;              //used set so that identical vector does not repeats.
+         set<vector<int>> s;
         vector<vector<int>> v;
         int left,right,sum=0;
          
         if(nums.size()<3){
             return v;
         }
-        sort(nums.begin(),nums.end());   //first,sort the array so that the -ve values occur first and the +ve values later.
+        sort(nums.begin(),nums.end());
         
-        for(int i=0;i<nums.size()-2;i++){         
+        for(int i=0;i<nums.size()-2;i++){
             
             left=i+1;right=nums.size()-1;sum=0-nums[i];
             
